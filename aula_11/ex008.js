a856b715fc9683b6def0fb21be219e96aece49b4
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2024-06-20 08:46:59 -0300

    Exercicio 8 do curso de Javascript

---
+++ aula_11/ex008.js
@@ -0,0 +1,8 @@
+var velocidade = 50.5
+console.log(`A velocidade do seu carro é ${velocidade}Km/h.`)
+
+if (velocidade > 60.0) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
