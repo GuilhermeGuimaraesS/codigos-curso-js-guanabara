1f2b87c7e1891b2e52b8675c15f92be1bc4d6e13
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2024-06-29 12:18:01 -0300

    Contagem regressiva adicionada

--- aula_14ex/ex016/script.js
+++ aula_14ex/ex016/script.js
@@ -22,24 +22,36 @@ function contar() {
     //     resultado.innerText += ` ${ primeiro_numero }`
     // }
     
+    // Verifica se foram digitados inicio e fim da contagem
     if (numero_1_txt.value.length <= 0 || numero_2_txt.value.length <= 0 ) {
         resultado.innerHTML = "Imposssível contar..."
 
+    // Verifica se o passo está vazio ou com o valor 0
     } else if (passo_txt.value.length <= 0 || passo_txt.value == 0) {
         passo = 1
         alert("Valor inválido para o passo! Considerando PASSO = 1")
         resultado.innerHTML = `Contando: `
         while (primeiro_numero <= segundo_numero) {
             // alert(primeiro_numero)
-            contagem.innerHTML += ` ${primeiro_numero}`
+            contagem.innerHTML += ` ${primeiro_numero} ${emoji_apontando}`
             primeiro_numero += passo
             
-        }
+        } contagem.innerHTML += `${emoji_chegada}`
 
+    // Faz contagem regressiva
+    } else if (segundo_numero < primeiro_numero) {
+        resultado.innerHTML = `Contando: `
+        while (primeiro_numero >= segundo_numero) {
+            contagem.innerHTML += ` ${primeiro_numero} ${emoji_apontando}`
+            primeiro_numero -= passo
+
+        } contagem.innerHTML += `${emoji_chegada}`
+
+    // Faz a contagem normal
     } else {
         resultado.innerHTML = `Contando: `
         while (primeiro_numero <= segundo_numero) {
-            contagem.innerHTML += ` ${primeiro_numero} ${emoji_apontando}`
+            contagem.innerHTML += `${primeiro_numero} ${emoji_apontando}`
             primeiro_numero += passo
 
         } contagem.innerHTML += `${emoji_chegada}`
